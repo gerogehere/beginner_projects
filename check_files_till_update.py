179398b5580aa07cd097ad02abf4abece7f015af
george <119468024+gerogehere@users.noreply.github.com>
2025-05-20 15:20:16 +0200

    Add files via upload

---
+++ check_files_till_update.py
@@ -0,0 +1,14 @@
+import time
+
+keyword = input("Enter the Keyword to search for: ")
+while True:
+    with open("data.txt", "r") as f:
+        lines = f.readlines()
+
+        print("Reading file...")
+        # Check for something in the file
+        if keyword in "".join(lines):
+            print("Found the keyword!")
+        
+            break
+    time.sleep(1) 
