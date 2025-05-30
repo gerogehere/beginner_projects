61008009fe9d29f2f8e01ee2717094a224ccc22b
george <119468024+gerogehere@users.noreply.github.com>
2025-05-30 23:41:41 +0200

    Update market.py

--- market.py
+++ market.py
@@ -4,42 +4,53 @@ wallet = 50
 
 credit_card = 30
 
-items = {
-    "apple": 0.5, "orange": 0.7, "chips": 1.09, "chesse": 2.05, "milk": 0.85, "sprite": 0.91, "juice": 1,
-    "biscuits": 1.44, "shampoo": 1.78, "chocolate": 1.9, "fish": 2.39, "frozen pizza": 2.45, "eggs": 2.32,
-    "fanta": 0.91, "watermelon": 1.99, "bread": 0.89, "cake": 2.89, "stake": 2.79, "tomatoes": 0.9, "watter": 0.79
+products = {
+    "apple": {'price': 0.5, 'stock': 9}, "orange": {'price': 0.7, 'stock': 7}, "chips": {'price': 1.09, 'stock': 4}, "chesse": {'price': 2.05, 'stock': 0},
+      "milk": {'price': 0.85, 'stock': 14}, "sprite": {'price': 0.91, 'stock': 8}, "juice":{'price': 1, 'stock': 15},"biscuits": {'price': 1.44, 'stock': 2}, 
+      "shampoo": {'price': 1.78, 'stock': 9}, "chocolate": {'price': 1.9, 'stock': 3}, "fish": {'price': 2.39, 'stock': 15}, "frozen pizza": {'price': 2.45, 'stock': 6},
+    "eggs": {'price': 2.32, 'stock': 10}, "fanta":{'price': 0.91, 'stock': 4}, "watermelon": {'price': 1.99, 'stock': 15}, "bread":{'price': 0.89, 'stock': 6}, 
+    "cake": {'price': 2.89, 'stock': 3}, "stake":{'price': 2.79, 'stock': 3}, "tomatoes":{'price': 0.9, 'stock': 17}, "watter":{'price': 0.79, 'stock': 20}
 }
 
-def total_sum_selected(items, list_of_items_in_cart):
-    global total
+
+def displayinvetory(products):
+ for item, details, in products.items():
+    print(f"{item}: ${details['price']} {details["stock"]}")
+
+
+displayinvetory(products)
+def total_sum_selected(products, list_of_items_in_cart):
     total = 0
-    for item in list_of_items_in_cart:
-        if item in items:
-            total += items[item]
+    for item in list_of_items_in_cart:  
+        if item in products:  
+            total += products[item]['price']
     return total
 
-def displayinvetory(inventory):
-    print("Hello! Today We Offer:")
-    for k, v in inventory.items():
-        print(f"{k} for {v}$")
-
-displayinvetory(items)
-
+def check_stock(products, user_choice):
+    if products[user_choice]['stock'] == 0:
+        print(f"We're sorry but {user_choice} is out of stock")
+        return False
+    return True
+          
+                
 while True:
-    user_choise = input('"q" to checkout ) Items you Want to Buy? ').lower()
 
-    if user_choise in items:
-        list_of_items_in_cart.append(user_choise)
-        print("This Item was added to your cart")
-        print("Items in your Card:", list_of_items_in_cart)
+    user_choice = input('"q" to checkout ) Items you Want to Buy? ').lower()
 
-    elif user_choise == "q":
-        print("this will be:", total_sum_selected(items, list_of_items_in_cart), "$")
-        user_pay_choise = input("How would you like to pay Cash or Credit Card? ").lower()
+    if user_choice in products:
+       if check_stock(products, user_choice): 
+           list_of_items_in_cart.append(user_choice)
+           print("Items in your Card:", list_of_items_in_cart)
+       else:
+           # Don't try to remove if we never added it
+           print("Items in your Card:", list_of_items_in_cart)
+    elif user_choice == "q":
+        print("this will be:", total_sum_selected(products, list_of_items_in_cart), "$")
+        user_pay_choice = input("How would you like to pay Cash or Credit Card? ").lower()
 
-        total = total_sum_selected(items, list_of_items_in_cart)
+        total = total_sum_selected(products, list_of_items_in_cart)
 
-        if user_pay_choise == "cash":
+        if user_pay_choice == "cash":
             wallet_after_pay = wallet - total
             print("Current Wallet Balance:", wallet_after_pay, "$")
             print("here's your receipt:")
@@ -47,7 +58,7 @@ while True:
                 print(x)
             print("Thank you for Shopping")
 
-        elif user_pay_choise == "credit card":
+        elif user_pay_choice == "credit card":
             credit_card_after_pay = credit_card - total
             print("Credit Card Current Balance:", credit_card_after_pay, "$")
             print("here's your receipt:")
@@ -60,3 +71,4 @@ while True:
     else:
         print("item is not in our Inventory!")
         print(list_of_items_in_cart)
+
